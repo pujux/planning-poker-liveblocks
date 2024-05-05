8b708554acaa935927b9ef759688cf71128f5be6
Julian Pufler <julian.pufler@lean-coders.at>
2024-05-05 16:30:18 +0200

    add favicon and logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { adjectives, colors, uniqueNamesGenerator, animals } from "unique-names-generator";
+import headerImage from "./planning-poker-trex.webp";
 
 export default function Page() {
   const router = useRouter();
@@ -10,7 +12,8 @@ export default function Page() {
   };
 
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen p-24">
+    <main className="flex flex-col items-center justify-center min-h-screen p-12 md:p-24">
+      <Image src={headerImage} alt="" width={300} height={300} />
       <h1 className="my-8 text-5xl font-bold text-center uppercase">Planning Poker</h1>
       <button
         className="px-4 py-2 text-gray-100 transition-colors bg-gray-500 border border-gray-300 rounded-md shadow-md hover:bg-gray-400"
